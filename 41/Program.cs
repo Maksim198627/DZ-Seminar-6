64e802e00a22edfcd6baace3cf1801bf7886ec16
Максим <Mt.Pm@yandex.ru>
2022-11-19 10:13:28 +0700

    Задачи на отправку

---
+++ 41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+System.Console.Write("Введите число элементов массива: ");// Ввод кол-ва элементов массива
+int elcount = int.Parse(Console.ReadLine()!);
+int[] array = Mass(elcount);
+int count = PositiveNumbers(elcount);
+System.Console.WriteLine();
+System.Console.WriteLine("Кол-во положительных чисел в массиве: " + count);
+
+
+
+int[] Mass(int elcount) // функция на рандомное заполнение массива
+{
+    int[] array1 = new int[elcount];
+    Random random = new Random();
+    System.Console.Write("Сформированный массив:");
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = random.Next(-10, 30);
+        System.Console.Write(array1[i]);
+
+        if (i < array1.Length - 1) System.Console.Write(",");
+
+    }
+    return array1;
+
+}
+
+int PositiveNumbers(int elcount)
+{
+    int sum = 0;
+    int[] arrab = new int[elcount];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+
+    }
+    return sum;
+
+}
---
+++ 43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// Значения b1, k1, b2 и k2 задаются пользователем.
+
+System.Console.Write("Введите точку прямой k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку прямой b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите точку прямой k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку прямой b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine();
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+System.Console.WriteLine($"Пересечение произойдет в точке: ({x};{y})");
+
+System.Console.WriteLine("Для продолжения нажмите любую кнопку: ");
+Console.ReadKey();
